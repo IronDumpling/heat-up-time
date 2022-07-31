953154ddacc340c6abef9658833083d55ec10ab9
IronDumpling <zhangchuyue.zhang@mail.utoronto.ca>
2022-07-31 18:27:36 -0400

    v0.1.2

---
+++ Assets/Scripts/Heat System/PlaneVillainHeat.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneVillainHeat : MonoBehaviour
+{
+    // Component pointers
+    private Collider2D coll;
+    private GameObject collideObj;
+    // Heat Value
+    public float curHeat;
+    public float boundHeat;
+    // Layers
+    public LayerMask bulletLayer;
+    public LayerMask planeLayer;
+    public LayerMask villainLayer;
+    public LayerMask playerLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Method 1. Get colliding object
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collideObj = collision.gameObject;
+
+        if (coll.IsTouchingLayers(playerLayer))
+        {
+            float otherHeat = collideObj.GetComponent<PlayerHeat>().curHeat;
+            if (otherHeat != curHeat)
+            {
+                HeatTransfer(otherHeat);
+            }
+        }
+
+        else if (coll.IsTouchingLayers(bulletLayer))
+        {
+            float otherHeat = collideObj.GetComponent<BulletController>().curHeat;
+            if (otherHeat != curHeat)
+            {
+                HeatGain(otherHeat);
+            }
+        }
+
+        else if (coll.IsTouchingLayers(planeLayer) ||
+            coll.IsTouchingLayers(villainLayer))
+        {
+            float otherHeat = collideObj.GetComponent<PlaneVillainHeat>().curHeat;
+            {
+                HeatTransfer(otherHeat);
+            }
+        }
+    }
+
+    // Method 2. Heat Transfer
+    void HeatTransfer(float otherHeat)
+    {
+        curHeat = (curHeat + otherHeat) / 2;
+    }
+
+    // Method 2. Heat Gain
+    void HeatGain(float otherHeat)
+    {
+        curHeat += otherHeat;
+    }
+}
